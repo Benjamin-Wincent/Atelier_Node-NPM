1e7f4f8e713bbf3e7e7eaa607036be6a8c2c66aa
Benjamin Vincent <benjamin.rm.vincent@gmail.com>
2019-03-20 11:16:12 +0100

    Dernier arrivage

---
+++ main.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('MICKEAL'));
+console.log(chalk.red('DIMITRI'));
+console.log(chalk.green('MAXENCE'));
+console.log(chalk.yellow('BENJAMIN'));
